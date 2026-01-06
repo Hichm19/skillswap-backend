492b832f3bbcfec735d0b60e0165bfa8fa5afbe5
hicham <chamackgaba@gmail.com>
2026-01-06 15:06:19 +0100

    routes de messages

--- routes/api.php
+++ routes/api.php
@@ -17,22 +17,21 @@ Route::middleware('auth:sanctum')->group(function(){
 
     // Envoyer une demande d'ami
     Route::post('/friend-requests', [FriendRequestController::class, 'store']);
-
     // Voir les demandes reçues
     Route::get('/friend-requests', [FriendRequestController::class, 'index']);
-
     // Voir une demande spécifique
     Route::get('/friend-requests/{friendRequest}', [FriendRequestController::class, 'show']);
-
     // Accepter / refuser une demande
     Route::put('/friend-requests/{friendRequest}', [FriendRequestController::class, 'update']);
-
     // Annuler / supprimer
     Route::delete('/friend-requests/{friendRequest}', [FriendRequestController::class, 'destroy']);
 
     Route::get('/matches', [UserMatchController::class, 'index']);
-
     Route::get('/matches/{userMatch}', [UserMatchController::class, 'show']);
-
     Route::delete('/matches/{userMatch}', [UserMatchController::class, 'destroy']);
+
+
+    Route::get('/matches/{match}/messages', [MessageController::class, 'index']);
+    Route::post('/matches/{match}/messages', [MessageController::class, 'store']);
+    Route::delete('/messages/{message}', [MessageController::class, 'destroy']);
 });
