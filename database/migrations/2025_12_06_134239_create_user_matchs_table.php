94440bb655b8afe98695ad20787c4a97a7e707e0
hicham <chamackgaba@gmail.com>
2026-01-13 20:26:47 +0100

    matchs utilisateurs

--- database/migrations/2025_12_06_134239_create_user_matchs_table.php
+++ database/migrations/2025_12_06_134239_create_user_matchs_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('matched_user_id')->constrained('users')->onDelete('cascade');
-            $table->float('score');
+            $table->double('score')->nullable();
             $table->timestamps();
         });
     }
